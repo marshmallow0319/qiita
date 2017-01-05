74400e1be39f5a72fc21a92dd267ffd9006a3c81
tech-camp <marshmallow0319@gmail.com>
2017-01-05 21:03:36 +0900

    user model 編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :articles
+  has_many :comments
+  has_many :stocks
+  validates :username, :email, :password, presence: true
 end
