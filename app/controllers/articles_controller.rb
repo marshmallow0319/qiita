604937fef4df6c4fee0c16b80f276d4c3a986272
tech-camp <marshmallow0319@gmail.com>
2017-01-08 04:05:54 +0900

    サインイン・サインアウト後の動きを追記

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,13 @@
 class ArticlesController < ApplicationController
+  before_action :move_to_index, except: :index
+  before_action :authenticate_user!, only: :new
 
   def index
+  end
+
+  private
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
