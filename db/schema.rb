619476d66022b97d32250d25a592ad4c8e317a0d
tech-camp <marshmallow0319@gmail.com>
2017-01-11 02:32:06 +0900

    articlesテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105120701) do
+ActiveRecord::Schema.define(version: 20170110171627) do
+
+  create_table "articles", force: :cascade do |t|
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "user_id",    limit: 4
+    t.string   "title",      limit: 255
+    t.text     "text",       limit: 65535
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
