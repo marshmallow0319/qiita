6513960141c2b6fb520dae64317026806bd48b9d
tech-camp <marshmallow0319@gmail.com>
2017-01-13 03:59:16 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'articles#index'
   devise_for :users
   resources :users, only: [:show, :edit, :update]
-  resources :articles, only: [:index]
+  resources :articles
 
 end
