47600d40e3f430d375abeb97e0682e745601f2ef
tech-camp <marshmallow0319@gmail.com>
2017-01-13 03:47:32 +0900

    プロフィールアップデート時の処理を追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,12 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update(update_params)
+      bypass_sign_in @user
+      redirect_to edit_user_path(current_user)
+    else
+      render :edit
+    end
   end
 
   private
@@ -15,4 +21,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update_params
+    params.require(:user).permit(:id, :username, :email, :email_checked, :password, :firstname, :lastname, :website, :company, :residence, :profile, :facebook_id, :linkedin_id, :google_plus_id, :avatar)
+  end
 end
