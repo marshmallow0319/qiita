a09ba613491969f863e0c2ac70b770d0588876a3
tech-camp <marshmallow0319@gmail.com>
2017-01-15 17:49:57 +0900

    コールバックコントローラ作成

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,27 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  # include Devise::Controllers::Rememberable
+
+  def twitter
+    callback_from :twitter
+  end
+
+  private
+  def callback_from(provider)
+    provider = provider.to_s
+
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user, event: :authentication
+    else
+      unless provider == 'twitter'
+        session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      else
+        session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
+      end
+      redirect_to new_user_registration_url
+    end
+  end
+end
