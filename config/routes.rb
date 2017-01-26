6b1c50784b1987faf8045649f4bfe2ff8e1d24b7
tech-camp <marshmallow0319@gmail.com>
2017-01-27 03:58:05 +0900

    ルーティング追記

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   resources :articles do
     resources :stocks, only: [ :create, :destroy]
+    resources :likes, only: [:create, :destroy]
   end
   resources :stocks, only: :index
 
