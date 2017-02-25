5e769d66a714982c1dcde300be14b97d915ec2de
tech-camp <marshmallow0319@gmail.com>
2017-02-25 17:11:58 +0900

    ルーティングにliker追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   root 'articles#index'
+
   devise_for :users, controllers: {
   omniauth_callbacks: 'users/omniauth_callbacks'
 }
-  resources :users, only: [:show, :edit, :update]
+
+  resources :users, only: [:show, :edit, :update] do
+    resources :likes, only: :index
+  end
   resources :articles do
     resources :stocks, only: [ :create, :destroy]
     resources :likes, only: [:create, :destroy]
+    resources :likers, only: :index
   end
   resources :stocks, only: :index
 
